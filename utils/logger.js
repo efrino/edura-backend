1ee90db773c5fac4f15d0ff3df08af9cd11e5ec5
Efrino Wahyu Eko Pambudi <efrinowep@gmail.com>
2025-07-01 05:57:04 +0700

    fix a logger

--- utils/logger.js
+++ utils/logger.js
@@ -1,14 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-
-const logFilePath = path.join(__dirname, '..', 'logs', 'activity.log');
-
-function logActivity(label, message) {
-  const timestamp = new Date().toISOString();
-  const content = `[${timestamp}] [${label}] ${message}\n`;
-  fs.appendFile(logFilePath, content, (err) => {
-    if (err) console.error('[Logger Error]', err);
-  });
+function logActivity(action, message) {
+    const timestamp = new Date().toISOString();
+    const logMessage = `[${timestamp}] [${action}] ${message}`;
+    console.log(logMessage);
 }
 
 module.exports = { logActivity };
