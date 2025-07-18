73c4dc735c26539291df5fc9cca7b07b2e9dacab
Efrino Wahyu Eko Pambudi <efrinowep@gmail.com>
2025-07-18 13:13:02 +0700

    fix cron jobs v2

--- routes/trigger-jobs.js
+++ routes/trigger-jobs.js
@@ -1,3 +1,39 @@
+// module.exports = {
+//     name: 'trigger-jobs',
+//     version: '1.0.0',
+//     register: async function (server) {
+//         server.route({
+//             method: 'POST',
+//             path: '/trigger-jobs',
+//             options: {
+//                 auth: false,
+//                 description: 'Manual trigger worker generate-sessions',
+//                 tags: ['api', 'Worker'],
+//             },
+//             handler: async (request, h) => {
+//                 const { exec } = require('child_process');
+//                 const path = require('path');
+
+//                 const workerPath = path.join(__dirname, '../worker/generate-sessions-worker.js');
+
+//                 exec(`node ${workerPath}`, (error, stdout, stderr) => {
+//                     if (error) {
+//                         console.error(`Exec error: ${error}`);
+//                         return;
+//                     }
+//                     if (stderr) {
+//                         console.error(`Stderr: ${stderr}`);
+//                     }
+//                     console.log(`Stdout: ${stdout}`);
+//                 });
+
+//                 return h.response({ message: 'Worker di-trigger.' });
+//             },
+//         });
+//     },
+// };
+const runWorker = require('../worker/generate-sessions-worker');
+
 module.exports = {
     name: 'trigger-jobs',
     version: '1.0.0',
@@ -11,23 +47,13 @@ module.exports = {
                 tags: ['api', 'Worker'],
             },
             handler: async (request, h) => {
-                const { exec } = require('child_process');
-                const path = require('path');
-
-                const workerPath = path.join(__dirname, '../worker/generate-sessions-worker.js');
-
-                exec(`node ${workerPath}`, (error, stdout, stderr) => {
-                    if (error) {
-                        console.error(`Exec error: ${error}`);
-                        return;
-                    }
-                    if (stderr) {
-                        console.error(`Stderr: ${stderr}`);
-                    }
-                    console.log(`Stdout: ${stdout}`);
-                });
-
-                return h.response({ message: 'Worker di-trigger.' });
+                try {
+                    await runWorker(); // ⬅️ langsung panggil worker
+                    return h.response({ message: 'Worker selesai diproses' });
+                } catch (err) {
+                    console.error('Worker error:', err);
+                    return h.response({ error: 'Worker gagal' }).code(500);
+                }
             },
         });
     },
